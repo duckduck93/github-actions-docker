2e7dda36dc7d4734307c84eb33016ddac809ca42
duckduck <duckduck@kakao.com>
2022-05-07 02:06:06 +0900

    init

---
+++ src/main/java/com/actions/githubactionsdocker/GithubActionsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.actions.githubactionsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsDockerApplication.class, args);
+    }
+
+}
